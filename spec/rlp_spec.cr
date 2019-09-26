93b42c94a0a20d547eab6197df85a83fe56b0602
Zack Kollar <rallokkcaz@gmail.com>
2019-09-26 03:35:27 -0700

    Fixed function naming

--- spec/rlp_spec.cr
+++ spec/rlp_spec.cr
@@ -2,13 +2,13 @@ require "./spec_helper"
 
 describe RLP do
   it "encodes data properly" do
-    result = RLP.encode_spec_type(0xf0u8)
-    result.should eq("OWN ENCODING")
+    result = RLP.get_spec_type(0xf0u8)
+    result.should eq(:self_encoded)
   end
 
   it "should not encode unspecified formats" do
-    result = RLP.encode_spec_type(1337)
-    result.should eq("NOT IMPLEMENTED")
+    result = RLP.get_spec_type(1337)
+    result.should eq(:unimplemented)
   end
   # it "works" do
   #   false.should eq(true)
--- src/rlp.cr
+++ src/rlp.cr
@@ -9,7 +9,7 @@ module RLP
   alias Value = Int64 | Char
 
   # Encode a valid encoded input
-  def encode_spec_type(input : T) forall T
+  def get_spec_type(input : T) forall T
     buffer = IO::Memory.new(sizeof(UInt8))
 
     # **DEBUG INPUT TYPE**
@@ -18,15 +18,12 @@ module RLP
     # puts input
     #
 
-    msg = "NOT IMPLEMENTED"
-
     if typeof(input) == UInt8
       if input >= 0x00u8 || input < 0xFFu8
-        msg = "OWN ENCODING"
-        return msg
+        return :self_encoded
       end
     else
-      return msg
+      return :unimplemented
     end
   end
 end
